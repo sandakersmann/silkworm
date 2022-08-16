28513a1f233e10ed13d08bcbbc601e95ca5d72ad
battlmonstr <battlmonstr@users.noreply.github.com>
2022-08-16 13:20:59 +0200

    refactor rlpx crypto

--- sentry/silkworm/sentry/rlpx/auth/ecies_cipher.cpp
+++ sentry/silkworm/sentry/rlpx/auth/ecies_cipher.cpp
@@ -17,17 +17,13 @@
 #include "ecies_cipher.hpp"
 #include <cassert>
 #include <memory>
-#include <gsl/util>
-
-#include <openssl/aes.h>
-#include <openssl/evp.h>
-#include <openssl/hmac.h>
-#include <silkpre/sha256.h>
 
 #include <silkworm/common/endian.hpp>
 #include <silkworm/common/secp256k1_context.hpp>
 #include <silkworm/sentry/common/ecc_key_pair.hpp>
-#include <silkworm/sentry/common/random.hpp>
+#include <silkworm/sentry/rlpx/crypto/aes.hpp>
+#include <silkworm/sentry/rlpx/crypto/hmac.hpp>
+#include <silkworm/sentry/rlpx/crypto/sha256.hpp>
 
 namespace silkworm::sentry::rlpx::auth {
 
@@ -35,10 +31,7 @@ static const std::size_t kKeySize = 16;
 static const std::size_t kMacSize = 32;
 
 static Bytes kdf(ByteView secret);
-static Bytes aes_encrypt(ByteView plain_text, ByteView key, ByteView iv);
-static Bytes aes_decrypt(ByteView cipher_text, ByteView key, ByteView iv);
-static Bytes sha256(ByteView data);
-static Bytes hmac(ByteView key, ByteView data1, ByteView data2, ByteView data3);
+using namespace crypto;
 
 Bytes EciesCipher::compute_shared_secret(PublicKeyView public_key_view, PrivateKeyView private_key) {
     secp256k1_pubkey public_key;
@@ -62,7 +55,7 @@ EciesCipher::Message EciesCipher::encrypt_message(ByteView plain_text, PublicKey
     ByteView aes_key(shared_secret.data(), kKeySize);
     ByteView mac_key(&shared_secret[kKeySize], kKeySize);
 
-    Bytes iv = common::random_bytes(AES_BLOCK_SIZE);
+    Bytes iv = aes_make_iv();
 
     Bytes cypher_text = aes_encrypt(plain_text, aes_key, iv);
     Bytes mac = hmac(sha256(mac_key), iv, cypher_text, mac_extra_data);
@@ -89,13 +82,13 @@ Bytes EciesCipher::decrypt_message(const EciesCipher::Message& message, PrivateK
 }
 
 size_t EciesCipher::round_up_to_block_size(size_t size) {
-    return (size + AES_BLOCK_SIZE - 1) / AES_BLOCK_SIZE * AES_BLOCK_SIZE;
+    return aes_round_up_to_block_size(size);
 }
 
 size_t EciesCipher::estimate_encrypted_size(size_t size) {
     return size
         + SecP256K1Context::kPublicKeySizeUncompressed
-        + AES_BLOCK_SIZE
+        + kAESBlockSize
         + kMacSize;
 }
 
@@ -107,77 +100,6 @@ static Bytes kdf(ByteView secret) {
     endian::store_big_u32(data.data(), 1);
     data += secret;
     return sha256(data);
-}
-
-static Bytes aes_encrypt(ByteView plain_text, ByteView key, ByteView iv) {
-    assert(key.size() == kKeySize);
-    assert(iv.size() == AES_BLOCK_SIZE);
-
-    EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
-    auto _ = gsl::finally([ctx] { EVP_CIPHER_CTX_free(ctx); });
-
-    int ok = EVP_EncryptInit(ctx, EVP_aes_128_ctr(), key.data(), iv.data());
-    if (!ok)
-        throw std::runtime_error("Failed to init AES encryption");
-
-    Bytes::size_type max_size = (plain_text.size() + AES_BLOCK_SIZE - 1) / AES_BLOCK_SIZE * AES_BLOCK_SIZE;
-    Bytes cypher_text(max_size, 0);
-
-    int cypher_len = 0;
-    int remaining_len = 0;
-    EVP_EncryptUpdate(ctx, cypher_text.data(), &cypher_len, plain_text.data(), static_cast<int>(plain_text.size()));
-    EVP_EncryptFinal(ctx, cypher_text.data() + cypher_len, &remaining_len);
-
-    cypher_text.resize(static_cast<Bytes::size_type>(cypher_len) + static_cast<Bytes::size_type>(remaining_len));
-    return cypher_text;
-}
-
-static Bytes aes_decrypt(ByteView cipher_text, ByteView key, ByteView iv) {
-    assert(key.size() == kKeySize);
-    assert(iv.size() == AES_BLOCK_SIZE);
-
-    EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
-    auto _ = gsl::finally([ctx] { EVP_CIPHER_CTX_free(ctx); });
-
-    int ok = EVP_DecryptInit(ctx, EVP_aes_128_ctr(), key.data(), iv.data());
-    if (!ok)
-        throw std::runtime_error("Failed to init AES decryption");
-
-    Bytes plain_text(cipher_text.size(), 0);
-
-    int text_len = 0;
-    int remaining_len = 0;
-    EVP_DecryptUpdate(ctx, plain_text.data(), &text_len, cipher_text.data(), static_cast<int>(cipher_text.size()));
-    EVP_DecryptFinal(ctx, plain_text.data() + text_len, &remaining_len);
-
-    plain_text.resize(static_cast<Bytes::size_type>(text_len) + static_cast<Bytes::size_type>(remaining_len));
-    return plain_text;
-}
-
-static Bytes sha256(ByteView data) {
-    Bytes hash(32, 0);
-    silkpre_sha256(hash.data(), data.data(), data.size(), /* use_cpu_extensions = */ false);
-    return hash;
-}
-
-static Bytes hmac(ByteView key, ByteView data1, ByteView data2, ByteView data3) {
-    assert(key.size() == 32);
-
-    HMAC_CTX* ctx = HMAC_CTX_new();
-    auto _ = gsl::finally([ctx] { HMAC_CTX_free(ctx); });
-
-    int ok = HMAC_Init_ex(ctx, key.data(), static_cast<int>(key.size()), EVP_sha256(), nullptr);
-    if (!ok)
-        throw std::runtime_error("Failed to init HMAC");
-
-    HMAC_Update(ctx, data1.data(), data1.size());
-    HMAC_Update(ctx, data2.data(), data2.size());
-    HMAC_Update(ctx, data3.data(), data3.size());
-
-    Bytes hash(HMAC_size(ctx), 0);
-    HMAC_Final(ctx, hash.data(), nullptr);
-
-    return hash;
 }
 
 Bytes EciesCipher::serialize_message(const Message& message) {
@@ -202,7 +124,7 @@ Bytes EciesCipher::serialize_message(const Message& message) {
 
 EciesCipher::Message EciesCipher::deserialize_message(ByteView message_data) {
     const std::size_t key_size = SecP256K1Context::kPublicKeySizeUncompressed;
-    const std::size_t iv_size = AES_BLOCK_SIZE;
+    const std::size_t iv_size = kAESBlockSize;
     const std::size_t mac_size = kMacSize;
 
     const std::size_t min_size = key_size + iv_size + mac_size;
